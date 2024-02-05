745f03b67db94889eb654d328b0db27559d160d5
test <test@gmail.com.tw>
2024-02-05 15:13:22 +0800

    update

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 // main.js
-const webcam = document.getElementById('webcam');
+const video = document.getElementById('video');
+const overlay = document.getElementById('overlay');
 const constraints = { video: true };
 
 navigator.mediaDevices.getUserMedia(constraints)
-  .then((stream) => { webcam.srcObject = stream; })
-  .catch((err) => { console.error(err); });
-
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('./models')
-]).then(startVideo);
+    .then((stream) => {
+        video.srcObject = stream;
+        video.addEventListener('loadedmetadata', () => {
+            startVideo();
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 function startVideo() {
-  webcam.addEventListener('play', () => {
-    const canvas = document.createElement('canvas');
-    canvas.width = webcam.width;
-    canvas.height = webcam.height;
-    document.body.append(canvas);
+    video.addEventListener('play', () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = video.width;
+        canvas.height = video.height;
+        document.body.append(canvas);
 
-    const context = canvas.getContext('2d');
+        const context = canvas.getContext('2d');
 
-    const render = async () => {
-      context.drawImage(webcam, 0, 0, webcam.width, webcam.height);
-      const detections = await faceapi.detectAllFaces(webcam, new faceapi.TinyFaceDetectorOptions());
-      
-      for (const detection of detections) {
-        const box = detection.box;
-        context.strokeStyle = '#00FF00';
-        context.lineWidth = 2;
-        context.strokeRect(box.x, box.y, box.width, box.height);
-      }
+        const render = async () => {
+            context.drawImage(video, 0, 0, video.width, video.height);
+            const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
 
-      requestAnimationFrame(render);
-    };
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            for (const detection of detections) {
+                const box = detection.box;
+                context.strokeStyle = '#00FF00';
+                context.lineWidth = 2;
+                context.strokeRect(box.x, box.y, box.width, box.height);
+            }
 
-    render();
-  });
+            requestAnimationFrame(render);
+        };
+
+        render();
+    });
 }
